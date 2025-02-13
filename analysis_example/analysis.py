22a5822dd55583404ffce470cd446562d3cd048b
Bernardo L. Español <esp.bernardo@gmail.com>
2025-02-13 14:59:46 -0300

    docs(markov): update usage example with the new functions

--- analysis_example/analysis.py
+++ analysis_example/analysis.py
@@ -1,7 +1,9 @@
 from turbfpe.part0_preanalysis import preanalysis
 from turbfpe.part1_turbulence_analysis import turbulence_analysis
+from turbfpe.part2_markov import markov
 
 params_filename = './Renner_params.toml'
 
 preanalysis.exec_rutine(params_filename)
 turbulence_analysis.exec_rutine(params_filename)
+markov.exec_rutine(params_filename)
