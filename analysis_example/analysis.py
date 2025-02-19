71953b02b27684f03e876c400fdb5b6f097bb243
Bernardo L. Español <esp.bernardo@gmail.com>
2025-02-19 12:26:34 -0300

    docs: update usage example with markov and entropy rutines

--- analysis_example/analysis.py
+++ analysis_example/analysis.py
@@ -1,9 +1,11 @@
 from turbfpe.part0_preanalysis import preanalysis
 from turbfpe.part1_turbulence_analysis import turbulence_analysis
 from turbfpe.part2_markov import markov
+from turbfpe.part3_entropy import entropy
 
 params_filename = './Renner_params.toml'
 
 preanalysis.exec_rutine(params_filename)
 turbulence_analysis.exec_rutine(params_filename)
 markov.exec_rutine(params_filename)
+entropy.exec_rutine(params_filename)
