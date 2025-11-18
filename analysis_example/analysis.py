d52e50c0dfd431ac791881fdefbea02ea25cb26e
Bernardo L. Español <esp.bernardo@gmail.com>
2025-11-18 15:47:41 -0300

    chore: rutine -> routine

--- analysis_example/analysis.py
+++ analysis_example/analysis.py
@@ -5,7 +5,7 @@ from turbfpe.part3_entropy import entropy
 
 params_filename = './Renner_params.toml'
 
-preanalysis.exec_rutine(params_filename)
-turbulence_analysis.exec_rutine(params_filename)
-markov.exec_rutine(params_filename)
-entropy.exec_rutine(params_filename)
+preanalysis.exec_rouine(params_filename)
+turbulence_analysis.exec_rouine(params_filename)
+markov.exec_rouine(params_filename)
+entropy.exec_rouine(params_filename)
